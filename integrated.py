c07a487f012f070a4d8a32097ae8ee12fb0344ea
Shantanu Pramanik <srs.shaan@gmail.com>
2017-11-09 03:32:03 +0530

    Integration attempt

--- integrated.py
+++ integrated.py
@@ -1,5 +1,8 @@
-# -*- coding: utf-8 -*-
-
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+import pandas as pd
+import json
 import os
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
@@ -7,18 +10,89 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
-# the OAuth 2.0 information for this application, including its client_id and
-# client_secret.
 CLIENT_SECRETS_FILE = "client_secret.json"
 
-# This OAuth 2.0 access scope allows for full read/write access to the
-# authenticated user's account and requires requests to use an SSL connection.
+DEVELOPER_KEY = "AIzaSyB3OjFaaHL7yjV8B_SySi9wEYjR_513icQ"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
 SCOPES = ['https://www.googleapis.com/auth/youtube.force-ssl']
-YOUTUBE_API_SERVICE_NAME = 'youtube'
-YOUTUBE_API_VERSION = 'v3'
 
+query = input("Enter the search query : ")
+print("Please wait while we retrieve the videos from the server ...")
 
+argparser.add_argument("--q", help="Search here", default=query)
+argparser.add_argument("--max-results", help="Max results", default=25)
+args = argparser.parse_args()
+options = args
+
+youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+
+search_result = youtube.search().list(
+    q=options.q,
+    type="video",
+    part="id,snippet",
+    maxResults=options.max_results
+).execute()
+
+videos = {}
+
+for search_result in search_result.get("items", []):
+    if search_result["id"]["kind"] == "youtube#video":
+        videos[search_result["id"]["videoId"]] = search_result["snippet"]["title"]
+
+s = ','.join(videos.keys())
+
+videos_list_result = youtube.videos().list(
+    id=s,
+    part='id,statistics'
+).execute()
+
+res = []
+for i in videos_list_result['items']:
+    temp_res = dict(v_id=i['id'], v_title=videos[i['id']])
+    temp_res.update(i['statistics'])
+    res.append(temp_res)
+pd.DataFrame.from_dict(res)
+
+print('The list of videos has been retrieved ...')
+print('\nGive us a momment while we validate the JSON data ...')
+# Validating the JSON data
+result = str(res)
+result = result.replace(":\"", ":'");
+result = result.replace(": \"", ": '");
+
+result = result.replace("\",", "',");
+result = result.replace("\" ,", "' ,");
+
+result = result.replace("\"", "*#");
+
+result = result.replace("{'", "{\"");
+result = result.replace("{ '", "{ \"");
+
+result = result.replace("' :", "\" :");
+result = result.replace("':", "\":");
+
+result = result.replace(": '", ": \"");
+result = result.replace(":'", ":\"");
+
+result = result.replace("',", "\",");
+result = result.replace("' ,", "\" ,");
+
+result = result.replace(", '", ", \"");
+result = result.replace(",'", ",\"");
+
+result = result.replace("' }", "\" }");
+result = result.replace("'}", "\"}");
+result = result.replace("*#", "'");
+
+print('JSON data validated...')
+# Adding the comments to the videos
+
+print('\nStarting Retrieval of comments...')
+root = json.loads(result)
+
+outputRoot = []
+commentData = ""
 def get_authenticated_service():
     flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
     credentials = flow.run_console()
@@ -36,18 +110,9 @@ def print_response(response):
     response = response.replace(":'", ":\"");
     response = response.replace("',", "\",");
     response = response.replace("'}", "\"}");
+    commentData = response
     # print(response)
-    try :
-        text_file = open("commentsList.json", "w",newline='',encoding='utf8')
-        text_file.write(response)
-        text_file.close()
-    except FileNotFoundError as error:
-        print("There was an error writing to the file" + error)
-    else:
-        print("The list of comments was successffully written to the file 'commentsList.json'")
 
-# Build a resource based on a list of properties given as key-value pairs.
-# Leave properties with empty values out of the inserted resource.
 def build_resource(properties):
     resource = {}
     for p in properties:
@@ -107,10 +172,26 @@ def comment_threads_list_by_video_id(client, **kwargs):
     return print_response(response)
 
 
-if __name__ == '__main__':
-    # When running locally, disable OAuthlib's HTTPs verification. When
-    # running in production *do not* leave this option enabled.
+for video in root:
+    outputVideo = {}
+    outputVideo["videoId"] = video['v_id']
+    outputVideo["videoTitle"] = video['v_title']
+
+    # Retrieving the comments
+
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
     client = get_authenticated_service()
+    print("Retriieving comments for : " + outputVideo['videoId'] + " => " + outputVideo['videoTitle'] + "...")
+    comment_threads_list_by_video_id(client, part='snippet,replies', videoId=outputVideo['videoId'])
 
-    comment_threads_list_by_video_id(client, part='snippet,replies', videoId='XS6ysDFTbLU')
+    outputVideo["comments"] = commentData
+    outputRoot.append(outputVideo)
+
+try:
+    f = open("./finalData.json", "w", encoding='utf8')
+    f.write(str(outputRoot))
+    f.close()
+except FileNotFoundError as error:
+    print("There was an error writing to the file" + error)
+else:
+    print("The list of comments was successffully written to the file 'finalData.json'")
